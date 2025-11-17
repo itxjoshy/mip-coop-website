98e835fa0bc19f749558cc9bc3ec50d1ad88f2ac
itxjoshy <johnmanifest2017@gmail.com>
2025-11-17 09:45:03 +0100

    prevent scroll when menu is open - mobile

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-const hambugerMenu = document.querySelector('.hamburger-menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const hambugerMenu = document.querySelector(".hamburger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 const pathName = window.location.pathname;
 const pageName = pathName.split("/").pop();
+const body = document.body;
 
-
-hambugerMenu.addEventListener('click', () =>{
-    hambugerMenu.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-})
-if(pageName === "index.html"){
-    document.querySelector('.home').classList.add("activeLink");
-}
-if(pageName === "about.html"){
-    document.querySelector('.about').classList.add("activeLink");
-}
-if(pageName === "projects.html"){
-    document.querySelector('.projects').classList.add("activeLink");
-}
-if(pageName === "contact.html"){
-    document.querySelector('.contact').classList.add("activeLink");
-}
+hambugerMenu.addEventListener("click", () => {
+  hambugerMenu.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
+});
